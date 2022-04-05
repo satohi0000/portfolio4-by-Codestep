6d3c476fe9b2f1953da65ad867b4f81e54660d11
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-04-05 17:27:27 +0900

    deleate

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="l-header__form" method="get" action="<?php echo esc_url( home_url()); ?>">
-<input class="l-header__form__box" type="search"  name="s" id="s"value="<?php if(get_search_query()) echo get_search_query() ?>">
-<i class="fas fa-search" aria hidden="true"></i>
-<input class="l-header__form__botton" type="submit"  value="検索"></form>
